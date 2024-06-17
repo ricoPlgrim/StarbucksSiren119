709d1d727da62e7321c38d0144b845823faa085a
ricoPlgrim <121208475+ricoPlgrim@users.noreply.github.com>
2024-06-17 22:20:53 +0900

    필터수정

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -91,11 +91,8 @@ var filterUi = {
         if (selectedValues.length > 0) {
             localStorage.setItem('selectedValues', JSON.stringify(selectedValues));
             console.log(selectedValues); //선택한 벨류값들 setitem으로 값 넣기
-            window.location.href = '../../index.html';
+            window.history.back();
         }
-
-
-
     },
 
     btnResetClick: function(){
