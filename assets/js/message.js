1d7a990e12b47851a064a57b77b49bbe6962bf89
ricoPlgrim <rico@plgrim.com>
2024-06-21 09:56:15 +0900

    필터 오류 수정 및 재난문자 페이지 수정완료

--- assets/js/message.js
+++ assets/js/message.js
@@ -6,6 +6,9 @@ var _message;
 var _appointed;
 var _allTeam;
 var _allCheckboxes;
+var _teamCheckBox;
+var _messageDetail;
+var _twodepth;
 
 var alarmUi = {
 
@@ -15,25 +18,20 @@ var alarmUi = {
     create: function () {
         _w = $(window);
         _message = $( ".message" );
+        _messageDetail = $( ".message_detail" );
         _appointed = _message.find( "#appointed" );
-        _checkboxes = $('input[type=checkbox][name=check]');
-        _allTeam = $( "#allTeam" );
-        _allCheckboxes = $('.team_list input[type=checkbox]').not('#allTeam');
-
-        // $('.team_list.check_list input[type=checkbox]').not('#allTeam').change(function() {
-        //     if ($(this).is(':checked')) {
-        //         $('#allTeam').prop('checked', false);
-        //     }
-        // });
-
-
-
+        _checkboxes = _message.find('input[type=checkbox][name=check]');
+        _allTeam =  _message.find( "#allTeam" );
+        _allCheckboxes = _message.find('.team_list input[type=checkbox]').not('#allTeam');
+        _teamCheckBox = _messageDetail.find( ".team_checkbox" );
+        _twodepth = _messageDetail.find( ".cm_type_twodepth" );
     },
     addEvent: function () {
         _appointed.on( "change", this.appointedChage );
         _checkboxes.on( "change", this.checkboxChage );
         _allTeam.on( "change", this.allTeamClick );
         _allCheckboxes.on( "change", this.allCheckboxesChange );
+        _teamCheckBox.on('change', this.teamCheckBoxChage );
     },
     loadEvent: function () {
         this.create();
@@ -64,6 +62,18 @@ var alarmUi = {
     allCheckboxesChange: function(){
         if ($(this).is(':checked')) {
             $('#allTeam').prop('checked', false);
+        }
+    },
+
+    teamCheckBoxChage: function(){
+        // 체크박스의 부모 라벨 요소를 찾습니다.
+        var label = $(this).closest('label');
+        var labelText = label.text().trim();
+        console.log( labelText == "DM" );
+        if ($(this).is(':checked') && labelText == "DM"){
+            _twodepth.css( "display", "block" )
+        } else {
+            _twodepth.css( "display", "none" )
         }
     }
     
